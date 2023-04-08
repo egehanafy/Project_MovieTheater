a9af8d938e444b15a402a0aafca19ffe066c6853
egehanafy <egehanafy@gmail.com>
2023-04-08 17:41:01 +0300

    created console app for test

---
+++ MovieTheater/Porject.TestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using Project.BLL.Concrete;
+using System;
+
+namespace Porject.TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieRepository movieRepo = new MovieRepository();
+
+            foreach (var item in movieRepo.GetAll())
+            {
+                Console.WriteLine(item.MovieName);
+            }
+        }
+    }
+}
