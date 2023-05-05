0ed9235dfc97ad617dd23a1fb1a0eb7c58a00d3e
egehanafy <egehan.afyonlu@gmail.com>
2023-05-05 19:53:36 +0300

    create controller showtime

---
+++ MovieTheater/Project.MVC/Areas/Dashboard/Controllers/ShowTimeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.BLL.IntService;
+using Project.BLL.Service;
+using Project.Entity.Entity;
+using Project.MVC.Areas.Dashboard.ViewModels;
+using System.Linq;
+
+namespace Project.MVC.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class ShowTimeController : Controller
+    {
+        private readonly IShowTimeService _showTimeService;
+
+        public ShowTimeController(IShowTimeService showTimeService)
+        {
+            _showTimeService = showTimeService;
+        }
+        public IActionResult Index()
+        {
+            return View(_showTimeService.GetAllShowTime().ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ShowTimeVM showTimeVM)
+        {
+            ShowTime showTime = new ShowTime
+            {
+                Time = showTimeVM.Time,
+                MovieId = showTimeVM.Movie.Id,
+                HallId = showTimeVM.Hall.Id
+            };
+
+            TempData["result"] = _showTimeService.CreateShowTime(showTime);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var deleted = _showTimeService.FindShowTime(id);
+
+            if (deleted != null)
+            {
+                TempData["result"] = _showTimeService.DeleteShowTime(deleted);
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id)
+        {
+            var updated = _showTimeService.FindShowTime(id);
+            return View(updated);
+        }
+
+        [HttpPost]
+        public IActionResult Update(ShowTime showTime)
+        {
+            TempData["result"] = _showTimeService.UpdateShowTime(showTime);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MovieTheater/Project.MVC/Areas/Dashboard/ViewModels/ShowTimeVM.cs
@@ -0,0 +1,16 @@
+﻿using Project.Entity.Entity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.MVC.Areas.Dashboard.ViewModels
+{
+    public class ShowTimeVM
+    {
+        [Required(ErrorMessage = "Zaman bos gecilemez")]
+        public DateTime Time { get; set; }
+        [Required(ErrorMessage = "Film bos gecilemez")]
+        public Movie Movie { get; set; }
+        [Required(ErrorMessage = "Salon bos gecilemez")]
+        public Hall Hall { get; set; }
+    }
+}
